71d058cf780bdf886ffee8f49f5767bf13b1ac17
volador <so.volador@gmail.com>
2014-06-19 01:09:49 +0800

    添加测试用方法

--- test/org/j2auth/test/servlet/Home.java
+++ test/org/j2auth/test/servlet/Home.java
@@ -2,14 +2,20 @@ package org.j2auth.test.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.j2auth.filters.CookieVerifier;
 import org.j2auth.main.AuthContext;
+import org.j2auth.util.Encoder;
+import org.j2auth.util.SimpleEncoder;
 
 /**
  * test servlet
@@ -29,10 +35,47 @@ public class Home extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession(true);
-		AuthContext context = (AuthContext) session.getAttribute(AuthContext.SESSION);
-		PrintWriter out = response.getWriter();
-		out.println(context);
+		String methodName = request.getParameter("method");
+		try {
+			Method m = this.getMethod(methodName);
+			m.invoke(this, request,response);
+		}catch(Exception e){
+			response.getWriter().println("no such method:" + methodName);
+		}
+	}
+	
+	public void hello(HttpServletRequest req, HttpServletResponse res){
+		try {
+			res.getWriter().println("fuck");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setcookie(HttpServletRequest req, HttpServletResponse res) throws IOException{
+		Cookie c_name = new Cookie(CookieVerifier.USER_ACCOUNT,"so.volador@gmail.com");
+		Cookie c_pass = new Cookie(CookieVerifier.USER_PASSWORD,(new SimpleEncoder()).encode("fuck_pass"));
+		c_name.setPath("/");
+		c_pass.setPath("/");
+		res.addCookie(c_name);
+		res.addCookie(c_pass);
+		res.getWriter().println("done.");
+	}
+	
+	public void authinfo(HttpServletRequest req, HttpServletResponse res) throws IOException{
+		HttpSession s = req.getSession(true);
+		AuthContext ac = (AuthContext) s.getAttribute(AuthContext.SESSION);
+		res.getWriter().println(ac);
+	}
+	
+	private Method getMethod(String methodName){
+		Method m = null;
+		try {
+			m = this.getClass().getDeclaredMethod(methodName, HttpServletRequest.class,HttpServletResponse.class);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return m;
 	}
 
 	/**
